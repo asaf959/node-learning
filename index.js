6d4f2a7064c471d800f5f26453737fa397d377b5
asaf959 <asifkhan03133834@gmail.com>
2022-12-13 23:09:26 +0500

    worked on post listining

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const fs = require('fs')
+const http = require('http');
 
 // blocking, synchronous way
-const text = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(text)
-const textout = `this is what we know about the avocado: ${text}.\nCreated on ${Date.now()}`
-fs.writeFileSync('./txt/output.text', textout)
-console.log('File written!');
+// const text = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(text)
+// const textout = `this is what we know about the avocado: ${text}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.text', textout)
+// console.log('File written!');
 
-// non blocking , asynchronous way 
-fs.readFile('./txt/start.txt', 'utf-8', (err,data) => {
-console.log(data)
+// non blocking , asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err,data1) => {
+//   if (err) return console.log('Error !');
+
+//   fs.readFile(`./txt/${data1}`, 'utf-8', (err,data2) => {
+//     console.log(data2)
+//     fs.readFile(`./txt/${data1}`, 'utf-8', (err,data3) => {
+//       console.log(data3)
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('your file has been written')
+//       })
+//       })
+//     })
+// })
+
+// console.log('Will read file!')
+
+// server
+
+const server = http.createServer((req,res) => {
+  res.end("hello from the server")
 })
 
-console.log('Will read file!')
+server.listen(8000, '127.0.0.1', () => {
+  console.log("Listening to request on port 8000")
+})
