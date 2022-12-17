3bc26f5b6c15415ef150570b351034e4d22703a1
asaf959 <asifkhan03133834@gmail.com>
2022-12-17 12:55:24 +0500

    worked on simple api

--- index.js
+++ index.js
@@ -28,6 +28,24 @@ const url = require('url');
 
 // server
 
+const replace = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%ID%}/g, product.id)
+
+  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+  return output
+
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
@@ -35,14 +53,26 @@ const dataObj = JSON.parse(data)
 const server = http.createServer((req,res) => {
   console.log(req.url)
   const pathName = req.url;
+
+
+  // Overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end("this is the overview")
+    res.writeHead(200, {'Content-type' : 'text/html'})
+    const cardHtml = dataObj.map(el => replace(tempCard, el)).join('')
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml)
+    res.end(output)
+
+ 
+    // Product page
   } else if (pathName === '/product') {
-    res.end("this is the product")
+    res.end(tempProduct)
+
+
+    // Api
   } else if (pathName === '/api') {
       res.writeHead(200, {'Content-type' : 'application/json'})
       res.end(data)
-
+// Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
