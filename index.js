8fcfbe40f39424131ea82057550571e80e4a64c9
asaf959 <asifkhan03133834@gmail.com>
2022-12-17 14:14:54 +0500

    create modules folder

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const http = require('http');
 const url = require('url');
+const replace = require('./modules/replace')
 
 // blocking, synchronous way
 // const text = fs.readFileSync('./txt/input.txt', 'utf-8')
@@ -28,20 +29,7 @@ const url = require('url');
 
 // server
 
-const replace = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%IMAGE%}/g, product.image)
-  output = output.replace(/{%PRICE%}/g, product.price)
-  output = output.replace(/{%FROM%}/g, product.from)
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-  output = output.replace(/{%QUANTITY%}/g, product.quantity)
-  output = output.replace(/{%DESCRIPTION%}/g, product.description)
-  output = output.replace(/{%ID%}/g, product.id)
 
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-  return output
-
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8')
